d31adc0169e78d6b4e001bd3befd29cbbe6b0046
Prathap K <prathap.krishna@in.bosch.com>
2025-07-29 10:49:19 +0530

    add test case of left over size

--- tshirts.py
+++ tshirts.py
@@ -2,7 +2,7 @@
 def size(cms):
     if cms < 38:
         return 'S'
-    elif cms < 42:
+    elif cms > 38 and cms < 42:
         return 'M'
     else:
         return 'L'
@@ -10,5 +10,6 @@ def size(cms):
 
 assert(size(37) == 'S')
 assert(size(40) == 'M')
-assert(size(43) == 'L')
+assert(size(42) == 'L')
+assert(size(38) == 'M')
 print("All is well (maybe!)")
